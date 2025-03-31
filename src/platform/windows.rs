58db0848c669d4a16931c2e1ab81bc02a9a09c92
Paweł Gorgoń <pgorgon@hem-e.com>
2025-03-31 17:01:13 +0200

    refactor(windows): consolidate session handling into a unified manager
    Replaced `WindowsPlaybackInfo` and `WindowsPlaybackControl` with a single `WindowsPlatformGlobalSessionManager` to centralize session management. Updated methods to dynamically retrieve the current session, streamlining session access and improving maintainability.

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -26,13 +26,13 @@ impl WindowsPlatform {
     }
 }
 
-pub struct WindowsPlaybackInfo {
-    session: GlobalSystemMediaTransportControlsSession,
+pub struct WindowsPlatformGlobalSessionManager {
+    session_manager: GlobalSystemMediaTransportControlsSessionManager,
 }
 
 const UNIX_EPOCH_OFFSET: i64 = 116444736000000000;
 
-impl WindowsPlaybackInfo {
+impl WindowsPlatformGlobalSessionManager {
     async fn new() -> Result<Self, PlaybackError> {
         let session_manager_result = GlobalSystemMediaTransportControlsSessionManager::RequestAsync()
             .map_err(|e| PlaybackError::UnknownError(e.to_string()))?
@@ -41,20 +41,23 @@ impl WindowsPlaybackInfo {
 
         let session_manager = session_manager_result.map_err(|e| PlaybackError::UnknownError(e.to_string()))?;
 
-        let session = session_manager
-            .GetCurrentSession()
-            .map_err(|e| PlaybackError::UnknownError(e.to_string()))?;
+        Ok(Self { session_manager })
+    }
 
-        Ok(Self { session })
+    async fn get_session(&self) -> Result<GlobalSystemMediaTransportControlsSession, PlaybackError> {
+        let session = self.session_manager
+                          .GetCurrentSession()
+                          .map_err(|e| PlaybackError::UnknownError(e.to_string()))?;
+        Ok(session)
     }
 
     async fn get_media_properties(&self) -> Result<windows::Media::Control::GlobalSystemMediaTransportControlsSessionMediaProperties, PlaybackError> {
-        Ok(self.session.TryGetMediaPropertiesAsync()?.await?)
+        Ok(self.get_session().await?.TryGetMediaPropertiesAsync()?.await?)
     }
 }
 
 #[async_trait]
-impl PlaybackInfoProvider for WindowsPlaybackInfo {
+impl PlaybackInfoProvider for WindowsPlatformGlobalSessionManager {
     async fn get_current_track(&self) -> Result<Track, PlaybackError> {
         let props = self.get_media_properties()
                         .await
@@ -67,7 +70,8 @@ impl PlaybackInfoProvider for WindowsPlaybackInfo {
     }
 
     async fn get_timeline_info(&self) -> Result<Option<TimelineInfo>, PlaybackError> {
-        let timeline = self.session.GetTimelineProperties()?;
+        let session = self.get_session().await?;
+        let timeline = session.GetTimelineProperties()?;
         let position = timeline.Position()?;
         let last_update_time = timeline.LastUpdatedTime()?;
         let end_time = timeline.EndTime()?.Duration as f64 / 10_000_000.0;
@@ -81,7 +85,7 @@ impl PlaybackInfoProvider for WindowsPlaybackInfo {
 
         let position_sec = position.Duration as f64 / 10_000_000.0;
 
-        let playback_info = self.session.GetPlaybackInfo()?;
+        let playback_info = session.GetPlaybackInfo()?;
         let rate = get_rate(&playback_info);
 
         Ok(Some(TimelineInfo {
@@ -93,7 +97,8 @@ impl PlaybackInfoProvider for WindowsPlaybackInfo {
     }
 
     async fn is_playing(&self) -> Result<bool, PlaybackError> {
-        let playback_info = self.session.GetPlaybackInfo()?;
+        let session = self.get_session().await?;
+        let playback_info = session.GetPlaybackInfo()?;
         let is_playing = playback_info.PlaybackStatus()? == windows::Media::Control::GlobalSystemMediaTransportControlsSessionPlaybackStatus::Playing;
         Ok(is_playing)
     }
@@ -110,34 +115,30 @@ fn get_rate(playback_info: &windows::Media::Control::GlobalSystemMediaTransportC
     }
     playback_info.PlaybackRate().map(|rate| rate.Value().unwrap_or(1.0)).unwrap_or(1.0) as f32
 }
-pub struct WindowsPlaybackControl {
-    session: GlobalSystemMediaTransportControlsSession,
-}
-
 #[async_trait]
-impl PlaybackControlProvider for WindowsPlaybackControl {
+impl PlaybackControlProvider for WindowsPlatformGlobalSessionManager {
     async fn play(&self) -> Result<(), PlaybackError> {
-        self.session.TryPlayAsync()?.await?;
+        self.get_session().await?.TryPlayAsync()?.await?;
         Ok(())
     }
 
     async fn pause(&self) -> Result<(), PlaybackError> {
-        self.session.TryPauseAsync()?.await?;
+        self.get_session().await?.TryPauseAsync()?.await?;
         Ok(())
     }
 
     async fn stop(&self) -> Result<(), PlaybackError> {
-        self.session.TryStopAsync()?.await?;
+        self.get_session().await?.TryStopAsync()?.await?;
         Ok(())
     }
 
     async fn next_track(&self) -> Result<(), PlaybackError> {
-        self.session.TrySkipNextAsync()?.await?;
+        self.get_session().await?.TrySkipNextAsync()?.await?;
         Ok(())
     }
 
     async fn previous_track(&self) -> Result<(), PlaybackError> {
-        self.session.TrySkipPreviousAsync()?.await?;
+        self.get_session().await?.TrySkipPreviousAsync()?.await?;
         Ok(())
     }
 
@@ -153,19 +154,13 @@ impl PlatformBehavior for WindowsPlatform {
     }
 
     async fn initialize(&self) -> Result<PlatformContext, String> {
-        let playback_info = WindowsPlaybackInfo::new()
+        let playback_info = Arc::new(WindowsPlatformGlobalSessionManager::new()
             .await
-            .map_err(|e| e.to_string())?;
-
-        let session: GlobalSystemMediaTransportControlsSession = playback_info.session.clone();
-
-        let playback_control = WindowsPlaybackControl {
-            session,
-        };
+            .map_err(|e| e.to_string())?);
 
         Ok(PlatformContext {
-            info: Arc::new(playback_info),
-            control: Arc::new(playback_control),
+            info: playback_info.clone(),
+            control: playback_info,
         })
     }
 
