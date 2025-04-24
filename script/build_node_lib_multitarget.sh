f1e204ef6403e91dafad66b12727d3b4b67f1491
Paweł Gorgoń <pgorgon@hem-e.com>
2025-04-24 21:05:01 +0200

    refactor: update package names and build scripts for consistency
    Renamed package names to align with the new namespace '@hemspzoo' and updated references in all relevant files. Additionally, modified the build script to include artifact generation and publishing configuration.

--- script/build_node_lib_multitarget.sh
+++ script/build_node_lib_multitarget.sh
@@ -8,12 +8,21 @@ cd "$SCRIPT_DIR/.." || exit
 
 # Build only the `fsct_volumio_service` for aarch64 linux
 cross build --target aarch64-unknown-linux-gnu --release -p fsct-node-lib
+cp target/aarch64-unknown-linux-gnu/release/libfsct_node_lib.so ports/node/fsct-lib.linux-arm64-gnu.node
 
 # Build only the `fsct_volumio_service` for armv7hf linux
-cross build --target armv7-unknown-linux-gnueabihf --release -p fsct-node-lib
+#cross build --target armv7-unknown-linux-gnueabihf --release -p fsct-node-lib
+#cp target/armv7-unknown-linux-gnueabihf/release/libfsct_node_lib.so ports/node/fsct-lib.linux-armv7-gnueabihf.node
 
 # Build only the `fsct_volumio_service` for armhf linux
 cross build --target arm-unknown-linux-gnueabihf --release -p fsct-node-lib
+cp target/arm-unknown-linux-gnueabihf/release/libfsct_node_lib.so ports/node/fsct-lib.linux-arm-gnueabihf.node
 
 # Build only the `fsct_volumio_service` for x86_64 linux
 cross build --target x86_64-unknown-linux-gnu --release -p fsct-node-lib
+cp target/x86_64-unknown-linux-gnu/release/libfsct_node_lib.so ports/node/fsct-lib.linux-x64-gnu.node
+
+cd ports/node/
+
+napi artifacts -d .
+napi prepublish
