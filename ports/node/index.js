e01da073816ac7d47b90dad21701bbbe856940b3
Paweł Gorgoń <pgorgon@hem-e.com>
2025-04-24 22:18:50 +0200

    refactor: update native binding file paths and package names
    Updated file paths and package names from `fsct-node-lib` to `fsct-lib` and external dependencies to use the `@hemspzoo` scope. This ensures consistency across all bindings.

--- ports/node/index.js
+++ ports/node/index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, 'fsct-node-lib.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'fsct-lib.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.android-arm64.node')
+            nativeBinding = require('./fsct-lib.android-arm64.node')
           } else {
-            nativeBinding = require('fsct-node-lib-android-arm64')
+            nativeBinding = require('@hemspzoo/fsct-lib-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'fsct-node-lib.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'fsct-lib.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.android-arm-eabi.node')
+            nativeBinding = require('./fsct-lib.android-arm-eabi.node')
           } else {
-            nativeBinding = require('fsct-node-lib-android-arm-eabi')
+            nativeBinding = require('@hemspzoo/fsct-lib-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -63,13 +63,13 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, 'fsct-node-lib.win32-x64-msvc.node')
+          join(__dirname, 'fsct-lib.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.win32-x64-msvc.node')
+            nativeBinding = require('./fsct-lib.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('fsct-node-lib-win32-x64-msvc')
+            nativeBinding = require('@hemspzoo/fsct-lib-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -77,13 +77,13 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, 'fsct-node-lib.win32-ia32-msvc.node')
+          join(__dirname, 'fsct-lib.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.win32-ia32-msvc.node')
+            nativeBinding = require('./fsct-lib.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('fsct-node-lib-win32-ia32-msvc')
+            nativeBinding = require('@hemspzoo/fsct-lib-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
@@ -91,13 +91,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'fsct-node-lib.win32-arm64-msvc.node')
+          join(__dirname, 'fsct-lib.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.win32-arm64-msvc.node')
+            nativeBinding = require('./fsct-lib.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('fsct-node-lib-win32-arm64-msvc')
+            nativeBinding = require('@hemspzoo/fsct-lib-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -108,23 +108,23 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, 'fsct-node-lib.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'fsct-lib.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./fsct-node-lib.darwin-universal.node')
+        nativeBinding = require('./fsct-lib.darwin-universal.node')
       } else {
-        nativeBinding = require('fsct-node-lib-darwin-universal')
+        nativeBinding = require('@hemspzoo/fsct-lib-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, 'fsct-node-lib.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'fsct-lib.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.darwin-x64.node')
+            nativeBinding = require('./fsct-lib.darwin-x64.node')
           } else {
-            nativeBinding = require('fsct-node-lib-darwin-x64')
+            nativeBinding = require('@hemspzoo/fsct-lib-darwin-x64')
           }
         } catch (e) {
           loadError = e
@@ -132,13 +132,13 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, 'fsct-node-lib.darwin-arm64.node')
+          join(__dirname, 'fsct-lib.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.darwin-arm64.node')
+            nativeBinding = require('./fsct-lib.darwin-arm64.node')
           } else {
-            nativeBinding = require('fsct-node-lib-darwin-arm64')
+            nativeBinding = require('@hemspzoo/fsct-lib-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -152,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, 'fsct-node-lib.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'fsct-lib.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./fsct-node-lib.freebsd-x64.node')
+        nativeBinding = require('./fsct-lib.freebsd-x64.node')
       } else {
-        nativeBinding = require('fsct-node-lib-freebsd-x64')
+        nativeBinding = require('@hemspzoo/fsct-lib-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -168,26 +168,26 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-x64-musl.node')
+            join(__dirname, 'fsct-lib.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-x64-musl.node')
+              nativeBinding = require('./fsct-lib.linux-x64-musl.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-x64-musl')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-x64-gnu.node')
+            join(__dirname, 'fsct-lib.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-x64-gnu.node')
+              nativeBinding = require('./fsct-lib.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-x64-gnu')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -197,26 +197,26 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-arm64-musl.node')
+            join(__dirname, 'fsct-lib.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-arm64-musl.node')
+              nativeBinding = require('./fsct-lib.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-arm64-musl')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-arm64-gnu.node')
+            join(__dirname, 'fsct-lib.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-arm64-gnu.node')
+              nativeBinding = require('./fsct-lib.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-arm64-gnu')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -226,26 +226,26 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-arm-musleabihf.node')
+            join(__dirname, 'fsct-lib.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-arm-musleabihf.node')
+              nativeBinding = require('./fsct-lib.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-arm-musleabihf')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-arm-gnueabihf.node')
+            join(__dirname, 'fsct-lib.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-arm-gnueabihf.node')
+              nativeBinding = require('./fsct-lib.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-arm-gnueabihf')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -255,26 +255,26 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-riscv64-musl.node')
+            join(__dirname, 'fsct-lib.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-riscv64-musl.node')
+              nativeBinding = require('./fsct-lib.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-riscv64-musl')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, 'fsct-node-lib.linux-riscv64-gnu.node')
+            join(__dirname, 'fsct-lib.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./fsct-node-lib.linux-riscv64-gnu.node')
+              nativeBinding = require('./fsct-lib.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('fsct-node-lib-linux-riscv64-gnu')
+              nativeBinding = require('@hemspzoo/fsct-lib-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -283,13 +283,13 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, 'fsct-node-lib.linux-s390x-gnu.node')
+          join(__dirname, 'fsct-lib.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./fsct-node-lib.linux-s390x-gnu.node')
+            nativeBinding = require('./fsct-lib.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('fsct-node-lib-linux-s390x-gnu')
+            nativeBinding = require('@hemspzoo/fsct-lib-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
